76e116a2d0908a6ca8a6d487af0faf4ca45970b8
道宏 劉 <mushiking02134tc@gmail.com>
2021-08-03 17:56:25 +0800

    V 1.3.4 - Add user control add control practice

--- WebFormMiniSample/WebApplication1/ucAddControl.ascx
+++ WebFormMiniSample/WebApplication1/ucAddControl.ascx
@@ -1,1 +1,2 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ucAddControl.ascx.cs" Inherits="WebApplication1.ucAddControl" %>
+<asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
--- WebFormMiniSample/WebApplication1/ucAddControl.ascx.cs
+++ WebFormMiniSample/WebApplication1/ucAddControl.ascx.cs
@@ -9,21 +9,34 @@ namespace WebApplication1
 {
     public partial class ucAddControl : System.Web.UI.UserControl
     {
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            if (this.Session["ControlList"] != null)
+            {
+                Label lbl = new Label();
+                lbl.ID = "Lebel1";
+                lbl.Text = "Test";
+
+                TextBox txt = new TextBox();
+                txt.ID = "TextBox1";
+                txt.Text = "Test";
+
+                Button btn = new Button();
+                btn.ID = "Button2";
+                btn.Text = "Click";
+                btn.Click += btn_Click;
+
+                this.Controls.Add(lbl);
+                this.Controls.Add(txt);
+                this.Controls.Add(btn);
+
+                this.Session["ControlList"] = null;
+            }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            Label lbl = new Label();
-            lbl.Text = "Test";
 
-            TextBox txt = new TextBox();
-            txt.Text = "Test";
-
-            Button btn = new Button();
-            btn.Text = "Click";
-            btn.Click += btn_Click;
-
-            this.Controls.Add(lbl);
-            this.Controls.Add(txt);
-            this.Controls.Add(btn);
         }
 
         private void btn_Click(object sender, EventArgs e)
@@ -31,6 +44,28 @@ namespace WebApplication1
             // 新增項目 無法findcontrol
             // var txt = this.FindControl("txt1") as TextBox;
             // Response.Write(txt.Text);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //Label lbl = new Label();
+            //lbl.ID = "Lebel1";
+            //lbl.Text = "Test";
+
+            //TextBox txt = new TextBox();
+            //txt.ID = "TextBox1";
+            //txt.Text = "Test";
+
+            //Button btn = new Button();
+            //btn.ID = "Button2";
+            //btn.Text = "Click";
+            //btn.Click += btn_Click;
+
+            //this.Controls.Add(lbl);
+            //this.Controls.Add(txt);
+            //this.Controls.Add(btn);
+
+            this.Session["ControlList"] = new string[] { "Lebel1", "TextBox1", "Button2" };
         }
     }
 }
